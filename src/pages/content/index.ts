8dc13e197a896d979292d5fbdfb36c39606e4d9f
mochenya <2827075398@qq.com>
2023-11-30 17:49:57 +0800

    update content scripts

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,21 +1,17 @@
-import {createApp} from "vue";
-import {createPinia} from "pinia";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "@pages/content/index.css";
 import "@assets/styles/tailwind.css";
 import Content from "@pages/content/Content";
 
 const div = document.createElement("div");
-div.id = "__root";
+div.id = "VueWebExtensionApp";
 document.body.appendChild(div);
 
 const pinia = createPinia();
 const app = createApp(Content);
 
 app.use(pinia);
-app.mount("#__root");
+app.mount("#VueWebExtensionApp");
 
-try {
-  console.log("content script loaded");
-} catch (e) {
-  console.error(e);
-}
+console.log("content script loaded");
