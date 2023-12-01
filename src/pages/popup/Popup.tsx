87ac72bff7ad02812d109c08f5a929ff1a9e9105
mochenya <2827075398@qq.com>
2023-12-01 12:27:05 +0800

    content and popup page change logo

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -7,17 +7,12 @@ export default defineComponent({
     return () => (
       <div class="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
         <header class="flex flex-col items-center justify-center text-white">
-          <img src={logo} class="h-36 pointer-events-none animate-spin-slow" alt="logo"/>
+          <img src={logo} class="h-36 pointer-events-none animate-pulse" alt="logo"/>
           <p>
-            Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
+            Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
           </p>
-          <a
-            class="text-blue-400"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React!
+          <a class="text-blue-400" href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">
+            Learn Vue!
           </a>
           <p>Popup styled with TailwindCSS!</p>
         </header>
